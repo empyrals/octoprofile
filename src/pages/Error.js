8f2c07ba60d18d5013785494f95267c863e76398
Aditya <empyrals@gmail.com>
2021-06-21 11:55:28 +0530

    initial commit

---
+++ src/pages/Error.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+const Error = () => {
+  return (
+    <Wrapper>
+      <div>
+        <h1>Error 404</h1>
+        <h3>The Page you are trying to find, doesn't exist.</h3>
+        <Link>
+          <button className="btn">Back to home</button>
+        </Link>
+      </div>
+    </Wrapper>
+  );
+};
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background: var(--clr-primary-10);
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    color: var(--clr-grey-3);
+    margin-bottom: 1.5rem;
+  }
+`;
+export default Error;
